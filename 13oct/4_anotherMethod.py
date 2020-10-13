bfdf8fd487a4e6db0b19e0c11cd147001b2f984e
Shazib Rahman <45175156+shazx06@users.noreply.github.com>
2020-10-13 22:51:09 +0530

    Update 4_anotherMethod.py

--- 13oct/4_anotherMethod.py
+++ 13oct/4_anotherMethod.py
@@ -6,11 +6,6 @@ class Solution:
           stack=[le-1]
 
           for i in range(le-2, -1, -1):
-            if stack and temp[stack[-1]]> temp[i]:
-              res[i]=stack[-1]-i
-              stack.append(i)
-
-            else:
               while stack and temp[stack[-1]]<=temp[i]:
                 stack.pop()
               if stack:
