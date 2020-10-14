59024f4de97b7585f49282a941098a4f680f477d
Shazib Rahman <45175156+shazx06@users.noreply.github.com>
2020-10-14 18:31:08 +0530

    Update 2.py

--- 14oct/2.py
+++ 14oct/2.py
@@ -2,7 +2,7 @@ def func(ranked , player):
   out=[]
   ranked=list(set(ranked))
   ranked.sort(reverse=True)
-  print(ranked)
+
 
   i=0
   while i < len(player):
@@ -18,10 +18,3 @@ def func(ranked , player):
   return out
 
 
-
-print(func([100 ,100 ,50 ,40 ,40 ,20 ,10],[5 ,25 ,50 ,120]))
-
-def fu(a):
-  while a:
-    if a[-1]==a[-2]:
-      a.pop()
