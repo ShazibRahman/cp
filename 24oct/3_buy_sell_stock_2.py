5ace35cda9069c1357e45134794b941abb47723d
shazib <shazibrahman@outlook.com>
2021-01-21 12:14:03 +0530

    commit

--- 24oct/3_buy_sell_stock_2.py
+++ 24oct/3_buy_sell_stock_2.py
@@ -1,7 +1,9 @@
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
+    def maxProfit(self, prices) -> int:
         profit=0
         for i in range(len(prices)-1):
             if prices[i+1]>prices[i]:
                 profit+=prices[i+1]-prices[i]
         return profit
+
+print("hey")
